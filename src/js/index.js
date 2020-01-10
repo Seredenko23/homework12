9120f3d3a154adf79ff3517faffcee777072fd22
Vlad <srvladyk555@gmail.com>
2020-01-10 15:46:56 +0200

    init project

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+require("bootstrap");
+require("bootstrap/dist/css/bootstrap.min.css");
+require("./../css/style.css");
+console.log('bruh');
+console.log('bruh2');
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.ts',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: false }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'awesome-typescript-loader'
+          },
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
+};
